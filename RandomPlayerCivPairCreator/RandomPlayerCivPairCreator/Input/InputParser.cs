ff98c6802eb458f64c2b55f6aca103238320ffa4
Balogh Sergey Ferenc <baloghsergeyferenc@gmail.com>
2021-02-25 23:31:33 +0100

    Iteration 0 - Design

---
+++ RandomPlayerCivPairCreator/RandomPlayerCivPairCreator/Input/InputParser.cs
@@ -0,0 +1,13 @@
+﻿using RandomPlayerCivPairCreator.Model;
+using System;
+
+namespace RandomPlayerCivPairCreator.Input
+{
+    class InputParser
+    {
+        public PlayerCivPairModel Parse(string input)
+        {
+            throw new Exception();
+        }
+    }
+}
---
+++ RandomPlayerCivPairCreator/RandomPlayerCivPairCreator/Pairing/PairCreator.cs
@@ -0,0 +1,20 @@
+﻿using RandomPlayerCivPairCreator.Model;
+using System;
+
+namespace RandomPlayerCivPairCreator.Pairing
+{
+    class PairCreator
+    {
+        private readonly RandomGenerator m_RandomGenerator;
+
+        public PairCreator(RandomGenerator randomGenerator)
+        {
+            m_RandomGenerator = randomGenerator;
+        }
+
+        public void Create(PlayerCivPairModel model)
+        {
+            throw new Exception();
+        }
+    }
+}
