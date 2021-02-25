f45f2d403eab170017296305ef76d9b43511bed2
Balogh Sergey Ferenc <baloghsergeyferenc@gmail.com>
2021-02-25 23:25:26 +0100

    Iteration 2 - Implementation

--- RandomPlayerCivPairCreator/RandomPlayerCivPairCreator/Program.cs
+++ RandomPlayerCivPairCreator/RandomPlayerCivPairCreator/Program.cs
@@ -1,4 +1,10 @@
-﻿using System.Runtime.CompilerServices;
+﻿using RandomPlayerCivPairCreator.Result;
+using RandomPlayerCivPairCreator.Model;
+using RandomPlayerCivPairCreator.Pairing;
+using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using RandomPlayerCivPairCreator.Input;
 
 [assembly: InternalsVisibleTo("Test")]
 namespace RandomPlayerCivPairCreator
@@ -7,6 +13,17 @@ namespace RandomPlayerCivPairCreator
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("RandomPlayerGenerator executed.");
+            InputParser inputParser = new InputParser();
+            ResultHandler outputParser = new ResultHandler();
+            RandomGenerator randomGenerator = new RandomGenerator();
+            PairCreator pairCreator = new PairCreator(randomGenerator);
+
+            PlayerCivPairModel model = inputParser.Parse(args[0]);
+            pairCreator.Create(model);
+            string result = outputParser.Parse(model);
+            Console.WriteLine(result);
+            File.AppendAllText(args[1], result);
         }
     }
 }
